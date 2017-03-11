a7e12e7f1277143aa1cf4135c93b4f3454fa9cbc
Scott Barr <scottjbarr@gmail.com>
2017-03-11 16:45:01 +1000

    Refreshed elpa packages

---
+++ .emacs.d/elpa/f-20161002.800/f-autoloads.el
@@ -0,0 +1,15 @@
+;;; f-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("f.el") (22519 40932 347100 812000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; f-autoloads.el ends here
